c1f67d564a91d388eb6034fd394f936af7f9f1c5
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-01-25 01:10:13 +0100

    FindMinKeyGreaterThanTest

---
+++ src/test/java/org/schwefel/kv/test/FindMinKeyGreaterThanTest.java
@@ -0,0 +1,105 @@
+package org.schwefel.kv.test;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+
+import org.schwefel.kv.ForEachKeyValue;
+import org.schwefel.kv.KVStore;
+import org.schwefel.kv.StoreOps;
+
+public class FindMinKeyGreaterThanTest {
+
+    static final byte[] value = { 5, 5, 5, 5 };
+    static byte[] lowerBound = { 0, 0, 1, 0 };
+    static byte[] keyPrefix = { 1 };
+    //@formatter:off
+    static final byte[][] keys = { { 0, 0, 0, 0 },
+                                   { 0, 0, 0, 1 },
+                                   { 0, 0, 1, 0 }, // lowerBound
+                                   { 0, 0, 1, 1 },
+                                   { 0, 0, 1, 2 },
+                                   { 0, 0, 1, 3 },
+                                   { 0, 0, 1, 4 },
+                                   { 0, 0, 1, 5 },
+                                   { 0, 0, 1, 6 },
+                                   { 0, 1, 0, 0 },
+                                   { 0, 1, 1, 1 },
+                                   { 1, 1, 1, 1 }, // min
+                                   { 1, 1, 1, 2 },
+                                   { 1, 2, 0, 1 },
+                                   { 2, 1, 1, 1 },
+                                   { 2, 2, 1, 1 }};
+    //@formatter:on
+
+    public static void main(String[] args) {
+
+        try (StoreOps store = new KVStore(Paths.get("D:/Temp/rocksdb_database"))) {
+            for (int i = 0; i < keys.length; ++i) {
+                byte[] key = keys[i];
+                store.put(key, value);
+            }
+
+            // retrieve in key order
+            try (ForEachKeyValue kv = store.scanAll()) {
+                kv.forEachRemaining(new BiConsumer<byte[], byte[]>() {
+                    @Override
+                    public void accept(byte[] key, byte[] value) {
+                        System.out.println(Arrays.toString(key) + " / " + Arrays.toString(value));
+                    }
+                });
+            }
+
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            byte[] min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            lowerBound = new byte[] { 4, 4, 4, 4 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            keyPrefix = new byte[] { 9, 9, 9, 9, 9 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            keyPrefix = new byte[] { 1 };
+            lowerBound = new byte[] { 1, 2, 0, 1 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            keyPrefix = new byte[] { 0 };
+            lowerBound = new byte[] { 0, 0, 0, 0 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            keyPrefix = new byte[] { 2 };
+            lowerBound = new byte[] { 2, 1, 1, 1 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            lowerBound = new byte[] { 2, 2, 1, 1 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+
+            keyPrefix = new byte[] { 0, 1, 0, 0 };
+            lowerBound = new byte[] { 0, 1, 0, 1 };
+            System.out.println("smallest key starting with prefix " + Arrays.toString(keyPrefix)
+                    + " being strictly greater than " + Arrays.toString(lowerBound));
+            min = store.findMinKeyGreaterThan(keyPrefix, lowerBound);
+            System.out.println(Arrays.toString(min) + "\n");
+        }
+    }
+}
